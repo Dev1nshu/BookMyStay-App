6fe2548abdf2ba5b429cf4c9f2ac331ff3ab83df
Dev1nshu <devanshumali53@gmail.com>
2026-04-10 08:43:58 +0530

    Base application setup

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("\n**************************************");
+        System.out.println("        WELCOME TO BOOK MY STAY");
+        System.out.println("**************************************");
+        System.out.println("Discover amazing hotels and stays.");
+        System.out.println("Book your comfort in just a few clicks.");
+        System.out.println("Your journey begins with us!");
+        System.out.println("**************************************");
+    }
+}
